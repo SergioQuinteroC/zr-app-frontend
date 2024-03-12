106d8f2a6823e078fcc1a204797b664e8d916176
unknown <sergio.quinteroc@udea.edu.co>
2024-03-11 19:27:11 -0500

    Add routing

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -1,22 +1,28 @@
+import { useRoutes, BrowserRouter } from "react-router-dom";
+
 import "./App.css";
 import Home from "../Home";
 import Detail from "../Detail";
 import Dashboard from "../Dashboard";
+import Login from "../Login";
 import NotFound from "../NotFound";
-import SignIn from "../SignIn";
 
-function App() {
+const AppRoutes = () => {
+	const routes = useRoutes([
+		{ path: "/", element: <Home /> },
+		{ path: "/detail", element: <Detail /> },
+		{ path: "/dashboard", element: <Dashboard /> },
+		{ path: "/login", element: <Login /> },
+		{ path: "/*", element: <NotFound /> },
+	]);
+	return routes;
+};
+const App = () => {
 	return (
-		<>
-			<div className="text-center text-2xl font-bold">
-				<Home />
-				<Detail />
-				<Dashboard />
-				<NotFound />
-				<SignIn />
-			</div>
-		</>
+		<BrowserRouter>
+			<AppRoutes />
+		</BrowserRouter>
 	);
-}
+};
 
 export default App;
---
+++ src/Pages/Login/index.jsx
@@ -0,0 +1,5 @@
+function Login() {
+	return <div className="bg-red-100">Login</div>;
+}
+
+export default Login;
