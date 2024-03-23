025a3ccba3fb00c769f9a8e3cee3cabd086969e9
unknown <sergio.quinteroc@udea.edu.co>
2024-03-23 18:26:05 -0500

    Add filter estates

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -6,6 +6,7 @@ import Layout from "./../../Components/Layout";
 function Home() {
 	const [items, setItems] = useState();
 
+	console.log(items);
 	useEffect(() => {
 		fetch(`http://localhost:3000/api/v1/realestates`)
 			.then((response) => response.json())
@@ -15,9 +16,11 @@ function Home() {
 	return (
 		<Layout>
 			<div className="grid gap-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-				{items?.map((item) => (
-					<Card key={item.id} {...item} />
-				))}
+				{items
+					?.filter((item) => item.status == "active")
+					.map((item) => (
+						<Card key={item.id} {...item} />
+					))}
 			</div>
 			<RealestateDetail />
 		</Layout>
